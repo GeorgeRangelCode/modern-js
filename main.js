a3bec9d706b905101cb5263593d0a52128ac8c17
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2025-06-22 21:05:02 -0500

    02 - with()

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ console.log(animals.at(1)); // 🐶
 
 const arr = ["🦖", "🦕", "🐉", "🐓"];
 console.log(arr.at(-1)); // 🐓
+
+// with method
+const fruits = ["🍎", "🍌", "🍇", "🍊"];
+const newFruits = fruits.with(1, "🍓");
+console.log(newFruits); // [ '🍎', '🍓', '🍇', '🍊'}
+console.log(fruits); // [ '🍎', '🍌', '🍇', '🍊' ]
